a1f9ce152bf0fb20fc1cb907f9c4ce20dc515453
Morton Jonuschat <yabawock@gmail.com>
2012-08-03 23:21:29 +0200

    Make the base for encoding configurable

--- lib/hideous.rb
+++ lib/hideous.rb
@@ -6,41 +6,47 @@ module Hideous
     require 'hideous/xor_knuth_hash'
     extend ClassMethods
     include InstanceMethods
+
     cattr_accessor :hideous_prime
     cattr_accessor :hideous_prime_inverse
     cattr_accessor :hideous_rndxor
     cattr_accessor :hideous_enabled
+    cattr_accessor :hideous_base
+
     self.hideous_prime = (options[:prime] || hideous_default_prime)
     self.hideous_prime_inverse = (options[:prime_inverse] || hideous_default_prime_inverse)
     self.hideous_rndxor = (options[:rndxor] || hideous_default_rndxor)
     self.hideous_enabled = (options[:auto].nil? ? true : options[:auto])
+    self.hideous_base = (options[:base] || hideous_default_base)
   end
 
-  def self.hide(id, hideous_prime, hideous_prime_inverse, hideous_rndxor)
-    XorKnuthHash.hash(id, hideous_prime, hideous_prime_inverse, hideous_rndxor)
+  def self.hide(id, hideous_prime, hideous_prime_inverse, hideous_rndxor, base)
+    XorKnuthHash.hash(id, hideous_prime, hideous_prime_inverse, hideous_rndxor, base)
   end
 
-  def self.show(id, hideous_prime, hideous_prime_inverse, hideous_rndxor)
-    XorKnuthHash.reverse_hash(id, hideous_prime, hideous_prime_inverse, hideous_rndxor)
+  def self.show(id, hideous_prime, hideous_prime_inverse, hideous_rndxor, base)
+    XorKnuthHash.reverse_hash(id, hideous_prime, hideous_prime_inverse, hideous_rndxor, base)
   end
 
 
   module ClassMethods
     def find(*args)
       if has_obfuscated_id?
-        args[0] = Hideous.show(args[0], self.hideous_prime, self.hideous_prime_inverse, self.hideous_rndxor)
+        args[0] = Hideous.show(args[0], self.hideous_prime, self.hideous_prime_inverse, self.hideous_rndxor, self.hideous_base)
       end
       super(*args)
     end
 
     def find_by_obfuscated_id(*args)
-      args[0] = Hideous.show(args[0].to_s, self.hideous_prime, self.hideous_prime_inverse, self.hideous_rndxor)
+      args[0] = Hideous.show(args[0].to_s, self.hideous_prime, self.hideous_prime_inverse, self.hideous_rndxor, self.hideous_base)
       find(*args)
     end
 
     def has_obfuscated_id?
       self.hideous_enabled
     end
+
+    private
 
     def hideous_default_prime
       413_158_511
@@ -53,11 +59,15 @@ module Hideous
     def hideous_default_rndxor
       652_109_907
     end
+
+    def hideous_default_base
+      16
+    end
   end
 
   module InstanceMethods
     def obfuscated_id
-      Hideous.hide(self.id, self.hideous_prime, self.hideous_prime_inverse, self.hideous_rndxor)
+      Hideous.hide(self.id, self.hideous_prime, self.hideous_prime_inverse, self.hideous_rndxor, self.hideous_base)
     end
 
     def to_param
